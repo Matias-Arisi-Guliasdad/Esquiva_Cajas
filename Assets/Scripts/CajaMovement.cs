91f0921fd44f2779a8f7cc80e5a333e24dd8c99e
Matias-Arisi-Guliasdad <49960010@est.ort.edu.ar>
2026-05-07 10:18:09 -0300

    Caja Movement implemente

---
+++ Assets/Scripts/CajaMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMovement : MonoBehaviour
+{
+    public float VelocidadCaja = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,-VelocidadCaja * Time.deltaTime ,0);
+    }
+}
