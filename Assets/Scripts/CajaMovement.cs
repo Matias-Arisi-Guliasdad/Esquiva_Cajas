6974cf5f0ece815164115f68929bb127c55076b2
Matias-Arisi-Guliasdad <49960010@est.ort.edu.ar>
2026-05-07 11:40:56 -0300

    La caja vuelve a su lugar

--- Assets/Scripts/CajaMovement.cs
+++ Assets/Scripts/CajaMovement.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CajaMovement : MonoBehaviour
+
 {
+    public CajaMovement cajaMovementScript;
+    float initialYValue;
     public float VelocidadCaja = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+      initialYValue = transform.position.y;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(0,-VelocidadCaja * Time.deltaTime ,0);
+    }
+    public void MoveBoxToRandomPosition()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.y = initialYValue;
+        transform.position = newPosition;
     }
 }
